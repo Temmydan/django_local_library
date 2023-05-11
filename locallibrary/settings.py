ad2b28654ff7d2ddd67bb1baa15eafbde9e03479
Temmydan <114483911+Temmydan@users.noreply.github.com>
2023-05-11 15:34:22 +0100

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -156,7 +156,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ## For example, for a site URL is at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-CSRF_TRUSTED_ORIGINS = ['https://web-production-ad90.up.railway.app/']
+##CSRF_TRUSTED_ORIGINS = ['https://web-production-ad90.up.railway.app/']
 
 # During development/for this tutorial you can instead set just the base URL
-# CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
